9b38e7b4fc6894077139180134030d6492e6b150
Bob-Lander <fambonloui@gmail.com>
2019-12-19 10:41:01 +0000

    Initial commit

---
+++ main.lua
@@ -0,0 +1,8 @@
+function love.load()
+  xPos = 0
+  yPos = 0
+  playerWidth = 64
+  playerHeight = 64
+  playerSpeed = 200
+  submarineImage = love.graphics.newImage("resources/images/submarine.png")
+end
